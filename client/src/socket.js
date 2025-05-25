c71aeb16a6cdfc77edcfb60aaf4eae788b64a784
Sadha Shiva Aeshala <shivaaeshala@gmail.com>
2025-05-25 18:37:50 +0530

    ..

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,2 +1,3 @@
 import { io } from 'socket.io-client';
-export const socket = io("https://real-time-chat-application-production.up.railway.app/");
+// export const socket = io("https://real-time-chat-application-production.up.railway.app/");
+export const socket = io("https://invigorating-renewal-production.up.railway.app");
